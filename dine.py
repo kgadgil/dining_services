8a19c0fad686ddb7144ca2a1d730ddfddca056f1
punishment <krightangle@gmail.com>
2019-04-30 07:16:03 -0400

    getting csv into numpy arrays

--- dine.py
+++ dine.py
@@ -1,46 +1,26 @@
 import tensorflow as tf
+import numpy as np
+
+#ITERATOR_BATCH_SIZE = 2
+#NR_EPOCHS = 3
+train_path = 'data/f2018.csv'
+
+data = np.genfromtxt(train_path, dtype=None, delimiter=',', names=True) 
+print(data)
+
 
 #tf.enable_eager_execution()
 
 #dataset = tf.data.experimental.CsvDataset(
-#   "data/f2018.csv",
+#   train_path,
 #   [tf.string,
 #    tf.string,
 #    tf.string,
 #    tf.string
 #   ],
-#   select_cols=[0,1,2,3]  # Only parse last three columns
+#   select_cols=[0,1,2,3]  # Only parse last three columns    
 #)
 
 #for element in dataset:
 #    print(element)
 
-ITERATOR_BATCH_SIZE = 2
-NR_EPOCHS = 3
-train_path = 'data/f2018.csv'
-
-dataset = tf.data.experimental.CsvDataset(
-        train_path,
-        [tf.string,
-         tf.string,
-         tf.string,
-         tf.string
-            ],
-        header=True,
-        field_delim=' '
-        )
-
-dataset = dataset.map(lambda *x: tf.convert_to_tensor(x))
-dataset = dataset.batch(ITERATOR_BATCH_SIZE)
-
-with tf.Session() as sess :
-    for i in range (NR_EPOCHS):
-        print('\nepochs',i)
-        iterator = dataset.make_one_shot_iterator()
-        next_element = iterator.get_next()
-        while True:
-            try:
-                data_and_target = sess.run(next_element)
-            except tf.errors.OutOfRangeError:
-                break
-            print("\n\n",data_and_target)
