a690203ff82d46df7d87d5cbc8c4cdac3ba9e4b8
punishment <krightangle@gmail.com>
2019-05-01 18:19:17 -0400

    with cleaned data

--- dine.py
+++ dine.py
@@ -1,13 +1,19 @@
 import tensorflow as tf
 import numpy as np
+#from io import StringIO
+import csv
 
 #ITERATOR_BATCH_SIZE = 2
 #NR_EPOCHS = 3
 train_path = 'data/f2018.csv'
 
-data = np.genfromtxt(train_path, dtype=None, delimiter=',', names=True) 
+data = np.genfromtxt(train_path, delimiter=',') 
 print(data)
 
+data_tf = tf.convert_to_tensor(data, preferred_dtype=tf.string)
+sess = tf.InteractiveSession()
+print(data_tf.eval())
+sess.close()
 
 #tf.enable_eager_execution()
 
