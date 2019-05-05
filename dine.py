e2f83b7dddca94076949fcda4a16bc3a7f18b6cd
human <krightangle@gmail.com>
2019-05-04 20:05:26 -0400

    matplotlib graph1

--- dine.py
+++ dine.py
@@ -1,19 +1,57 @@
 import tensorflow as tf
 import numpy as np
-#from io import StringIO
 import csv
+import matplotlib.pyplot as plt
 
-#ITERATOR_BATCH_SIZE = 2
-#NR_EPOCHS = 3
 train_path = 'data/f2018.csv'
 
-data = np.genfromtxt(train_path, delimiter=',') 
-print(data)
+#Read CSV into list of lists
+with open(train_path, 'r') as f:
+    data = list(csv.reader(f, delimiter=','))
+    #print(data[:3])
+data_np = np.array(data)
 
-data_tf = tf.convert_to_tensor(data, preferred_dtype=tf.string)
-sess = tf.InteractiveSession()
-print(data_tf.eval())
-sess.close()
+#Convert last col (Qty Sold) to int data type
+for item in data[1:]:       #start iterating from 2nd row in order to leave Qty Sold as string
+    item[-1] = int(item[-1])
+#print(data[:3]) #print first three rows
+
+#Collect unique Major Groups, Family groups and menu item names
+#Use set to get unique, list for list format
+major_groups = list(set(data_np[1:-1:, 2]))
+family_groups = list(set(data_np[1:-1, 1]))
+menu_items = list(set(data_np[1:-1, 0]))
+qty_sold = data_np[1:-1, 3]
+#print('major groups\n', major_groups)
+#print('family groups\n', family_groups)
+#print('menu items\n', menu_items)
+
+print(len(data_np[1:-1,0]))
+print(len(qty_sold))
+
+idx_menu = np.arange(len(data_np[1:-1,0]))
+plt.bar(idx_menu, qty_sold)
+plt.xlabel('Menu Items', fontsize=5)
+plt.ylabel('Qty Sold', fontsize=5)
+plt.xticks(idx_menu, data_np[1:-1,0], fontsize=5, rotation=30)
+plt.title('Qty Sold per Menu Item')
+plt.show()
+
+
+#######################################
+#######################################
+#######################################
+#data_tf = tf.convert_to_tensor(data)
+#ITERATOR_BATCH_SIZE = 2
+#NR_EPOCHS = 3
+
+#data = np.genfromtxt(train_path, delimiter=',') 
+#print(data)
+
+#data_tf = tf.convert_to_tensor(data, preferred_dtype=tf.string)
+#sess = tf.InteractiveSession()
+#print(data_tf.eval())
+#sess.close()
 
 #tf.enable_eager_execution()
 
