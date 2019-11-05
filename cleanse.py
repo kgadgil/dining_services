91a867825fe9910352aa65d95ac3e349bb44cc81
punishment <krightangle@gmail.com>
2019-11-05 07:09:26 -0500

    try 1, try2 regression

--- cleanse.py
+++ cleanse.py
@@ -12,30 +12,37 @@ def _cleanseCsv(inFile, outFile) :
             if cntr == 5:
                 tokens = re.split("\s+", line)
                 date = tokens[2]
-                print tokens
+                #print tokens
                 #print(tokens)
+            elif cntr == 6:
+                tokens = re.split(":", line)
+                cafe = tokens[1]
+                cafe = cafe.replace(',,,,','').strip()
+                #print cafe
             elif cntr >= 8:
                 line = line.replace(",,", ",")
                 line = line.strip()
                 day = datetime.datetime.strptime(date, '%Y-%m-%d').weekday()
-                outFile.write(line + "," + date + "," + str(day)+"\n")
+                outFile.write(line + "," + date + "," + str(day)+ "," + cafe + "\n")
         cntr +=1
 
 def cleanseCsv(list):
     for path in list:
-        in = open(path,"r")
+        in1 = open(path,"r")
         p = path.split("/")
         print p[-1]
         p[-1] = "clean" + p[-1]
         delim='/'
         opPath = delim.join(p)
-        clean = open(opPath,"w")
+        opFile = open(opPath,"w")
+        _cleanseCsv(in1, opFile)
+        in1.close()
 def main():
     in1 = open("121516.csv","r")
     clean1 = open("output.csv","w")
     in2 = open("121516.csv","r")
     clean2 = open("output1.csv","w")
-    cleanseCsv(in1, clean1)
+    _cleanseCsv(in1, clean1)
     in1.close()
     in2.close()
 
